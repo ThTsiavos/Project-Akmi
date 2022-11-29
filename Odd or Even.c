5809d5a5fa48d6be99e5d5956b1c41c729b3bf48
ThTsiavos <117170288+ThTsiavos@users.noreply.github.com>
2022-11-29 14:16:03 +0200

    Add files via upload

---
+++ Odd or Even.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+int a;
+
+{printf ("enter a number\n");}
+scanf ("%d",&a);
+
+if (a % 2 == 0)
+{printf ("%d is an even number",a);}
+else
+{printf ("%d is an odd number",a);}
+
+return 0;
+
+}
