ae23c9eff460e90bd0867a3854bd7fc1d1857beb
ThTsiavos <117170288+ThTsiavos@users.noreply.github.com>
2022-11-29 14:58:16 +0200

    Add files via upload

---
+++ Print the letters of the Alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+char alphabet[] = {"a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"};
+char Alphabet[] = {"A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"};
+
+printf("The lowercase letters of the English Alphabet in order are:\n" "%s",alphabet);
+printf("\nThe Uppercase letters of the English Alphabet in order are:\n" "%s",Alphabet);
+return 0;}
